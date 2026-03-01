960136ab2c32056e6956546ba36933c0096f5e88
choiguen <daindj0@gmail.com>
2026-03-02 00:34:26 +0900

    feat: genai를 활용한 충청도 사투리 ai 생성

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from google import genai
-from google.genai import types
+from google import generativeai as genai
+from fastapi.responses import StreamingResponse, HTMLResponse
 from dotenv import load_dotenv
 from fastapi.templating import Jinja2Templates
 import os
@@ -8,30 +8,32 @@ import os
 load_dotenv()
 openai_api_key = os.getenv("GPT_API_KEY")
 
-client = genai.Client(api_key=openai_api_key)
+client = genai.configure(api_key=openai_api_key)
+model = genai.GenerativeModel(
+    "gemini-2.5-flash",
+    system_instruction="너는 농담을 잘하는 충청도 사투리가 강한 말을 돌려서 비난하기를 잘하는 50대 아저씨야. 유능한 코드리뷰어이며, 친절하고 재미있음."
+    )
 
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
-@app.get("/item")
-@app.get("/item")
-def root(text: str):
-    try:
-        # system 역할을 prompt 앞부분에 직접 넣기
-        prompt = (
-            "너는 농담을 잘하는 충청도 사투리가 강한 말을 돌려서 비난하기를 잘하는 50대 아저씨야. "
-            "유능한 코드리뷰어이며, 친절하고 재미있음.\n\n"
-            f"사용자 입력: {text}"
-        )
+@app.get("/", response_class=HTMLResponse)
+def root():
+    for item in genai.list_models():
+        print(item.name)
+    return templates.TemplateResponse("response.html", {"request": {}})
 
-        response = client.models.generate_content(
-            model="gemini-3-flash-preview",
-            contents=prompt  # 문자열 그대로
-        )
+@app.get("/stream")
+def root(text: str = ""):
+    prompt = text
+    def gemerate():
+        response = model.generate_content(
+            prompt,
+            stream=True
+            )
+        for chunk in response:
+            if chunk.text:
+                yield chunk.text
 
-        answer = response.text
-    except Exception as e:
-        answer = f"Error: {str(e)}"
-
-    return templates.TemplateResponse("response.html", {"request": {}, "response": answer})
+    return StreamingResponse(gemerate(), media_type="text/plain")
